7d43680c780b16f202d73b0b3fc5064831ed79cf
lzf <1658422730@qq.com>
2023-12-11 20:42:37 +0800

    处理train的数据

--- mian.py
+++ mian.py
@@ -3,7 +3,8 @@ from data import build_corpus
 def main():
     print("读取数据.....")
     train_word_lists, train_tag_lists, word2id, tag2id = build_corpus("train")
+    print(word2id, len(word2id))
 
 
-if __name__ == "__mian__":
+if __name__ == "__main__":
     main()
